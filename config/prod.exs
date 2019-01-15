08c2b401888e6154abb8a96afc02f8ae25ddb24d
Joe Pearson <joe.pearson@avid.com>
2019-01-14 22:14:26 -0800

    add docker support

--- config/prod.exs
+++ config/prod.exs
@@ -10,9 +10,12 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :myapp, MyappWeb.Endpoint,
-  http: [:inet6, port: System.get_env("PORT") || 4000],
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  http: [:inet6, port: {:system, "PORT"}],
+  url: [host: "localhost", port: {:system, "PORT"}],
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  server: true,
+  root: ".",
+  version: Application.spec(:myapp, :vsn)
 
 # Do not print debug messages in production
 config :logger, level: :info
